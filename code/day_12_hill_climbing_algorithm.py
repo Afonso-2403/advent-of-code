8c1368e96db467e7a2a0139aa7761a166d3e8748
Afonso-2403 <avsluz@gmail.com>
2023-01-07 18:05:30 +0100

    Day 12 (exercise 1 and 2)

--- code/day_12_hill_climbing_algorithm.py
+++ code/day_12_hill_climbing_algorithm.py
@@ -1,4 +1,4 @@
-INPUT_FILE = "../input_files/day_12_input_example.txt"
+INPUT_FILE = "../input_files/day_12_input.txt"
 
 class Node():
   def __init__(self, x, y, height, start_node=False, destination_node=False):
@@ -11,16 +11,30 @@ class Node():
     self.parent_coordinates = None
     self.distance_to_current_node = 0
 
-    # find out how to make print(Node()) be this
-    def __str__(self):
-      return f"Position: ({self.x},{self.y}) height = {self.height}"
+  def __str__(self):
+    return f"Position: ({self.x},{self.y}) height = {self.height}"
 
-def bread_first_search(destination_coordinates, terrain_map, number_rows, number_columns):
+def finish_search_condition(terrain_map, current_node_coordinates, number_columns, exercise_number):
+  if exercise_number == 1:
+    if terrain_map[current_node_coordinates[0] * number_columns + current_node_coordinates[1]].start_node == True:
+      return True
+
+  if exercise_number == 2:
+    if terrain_map[current_node_coordinates[0] * number_columns + current_node_coordinates[1]].height == 0:
+      return True
+
+  return False
+
+
+def bread_first_search(destination_coordinates, terrain_map, number_rows, number_columns, exercise_number):
+  terrain_map[destination_coordinates[0] * number_columns + destination_coordinates[1]].explored = True
   coordinates_nodes_to_expand = [destination_coordinates]
   coordinates_expanded_nodes = set()
+
   while True:
+
     for current_node_coordinates in coordinates_expanded_nodes:
-      if terrain_map[current_node_coordinates[0] * number_columns + current_node_coordinates[1]].start_node == True:
+      if finish_search_condition(terrain_map, current_node_coordinates, number_columns, exercise_number) == True:
         return current_node_coordinates
       coordinates_nodes_to_expand.append(current_node_coordinates)
     coordinates_expanded_nodes = set()
@@ -32,14 +46,6 @@ def bread_first_search(destination_coordinates, terrain_map, number_rows, number
             (node_to_expand_coordinates[0] - 1) * number_columns + node_to_expand_coordinates[1]
           ].height >= terrain_map[node_to_expand_coordinates[0] * number_columns + node_to_expand_coordinates[1]].height - 1
         ):
-          if terrain_map[(node_to_expand_coordinates[0] - 1) * number_columns + node_to_expand_coordinates[1]].explored == False:
-            terrain_map[(node_to_expand_coordinates[0] - 1) * number_columns + node_to_expand_coordinates[1]].explored = True
-            terrain_map[(node_to_expand_coordinates[0] - 1) * number_columns + node_to_expand_coordinates[1]].parent_coordinates = node_to_expand_coordinates
-            terrain_map[
-              (node_to_expand_coordinates[0] - 1) * number_columns + node_to_expand_coordinates[1]
-            ].distance_to_current_node = terrain_map[node_to_expand_coordinates[0] * number_columns + node_to_expand_coordinates[1]].distance_to_current_node + 1
-            coordinates_expanded_nodes.add(((node_to_expand_coordinates[0] - 1), node_to_expand_coordinates[1]))
-
           if (
             terrain_map[(node_to_expand_coordinates[0] - 1) * number_columns + node_to_expand_coordinates[1]].explored == True
             and terrain_map[
@@ -52,20 +58,20 @@ def bread_first_search(destination_coordinates, terrain_map, number_rows, number
             ].distance_to_current_node = terrain_map[node_to_expand_coordinates[0] * number_columns + node_to_expand_coordinates[1]].distance_to_current_node + 1
             coordinates_expanded_nodes.add(((node_to_expand_coordinates[0] - 1), node_to_expand_coordinates[1]))
 
+          if terrain_map[(node_to_expand_coordinates[0] - 1) * number_columns + node_to_expand_coordinates[1]].explored == False:
+            terrain_map[(node_to_expand_coordinates[0] - 1) * number_columns + node_to_expand_coordinates[1]].explored = True
+            terrain_map[(node_to_expand_coordinates[0] - 1) * number_columns + node_to_expand_coordinates[1]].parent_coordinates = node_to_expand_coordinates
+            terrain_map[
+              (node_to_expand_coordinates[0] - 1) * number_columns + node_to_expand_coordinates[1]
+            ].distance_to_current_node = terrain_map[node_to_expand_coordinates[0] * number_columns + node_to_expand_coordinates[1]].distance_to_current_node + 1
+            coordinates_expanded_nodes.add(((node_to_expand_coordinates[0] - 1), node_to_expand_coordinates[1]))
+
       if node_to_expand_coordinates[0] < number_rows - 1:
         if (
           terrain_map[
             (node_to_expand_coordinates[0] + 1) * number_columns + node_to_expand_coordinates[1]
           ].height >= terrain_map[node_to_expand_coordinates[0] * number_columns + node_to_expand_coordinates[1]].height - 1
         ):
-          if terrain_map[(node_to_expand_coordinates[0] + 1) * number_columns + node_to_expand_coordinates[1]].explored == False:
-            terrain_map[(node_to_expand_coordinates[0] + 1) * number_columns + node_to_expand_coordinates[1]].explored = True
-            terrain_map[(node_to_expand_coordinates[0] + 1) * number_columns + node_to_expand_coordinates[1]].parent_coordinates = node_to_expand_coordinates
-            terrain_map[
-              (node_to_expand_coordinates[0] + 1) * number_columns + node_to_expand_coordinates[1]
-            ].distance_to_current_node = terrain_map[node_to_expand_coordinates[0] * number_columns + node_to_expand_coordinates[1]].distance_to_current_node + 1
-            coordinates_expanded_nodes.add(((node_to_expand_coordinates[0] + 1), node_to_expand_coordinates[1]))
-
           if (
             terrain_map[(node_to_expand_coordinates[0] + 1) * number_columns + node_to_expand_coordinates[1]].explored == True
             and terrain_map[
@@ -78,20 +84,20 @@ def bread_first_search(destination_coordinates, terrain_map, number_rows, number
             ].distance_to_current_node = terrain_map[node_to_expand_coordinates[0] * number_columns + node_to_expand_coordinates[1]].distance_to_current_node + 1
             coordinates_expanded_nodes.add(((node_to_expand_coordinates[0] + 1), node_to_expand_coordinates[1]))
 
+          if terrain_map[(node_to_expand_coordinates[0] + 1) * number_columns + node_to_expand_coordinates[1]].explored == False:
+            terrain_map[(node_to_expand_coordinates[0] + 1) * number_columns + node_to_expand_coordinates[1]].explored = True
+            terrain_map[(node_to_expand_coordinates[0] + 1) * number_columns + node_to_expand_coordinates[1]].parent_coordinates = node_to_expand_coordinates
+            terrain_map[
+              (node_to_expand_coordinates[0] + 1) * number_columns + node_to_expand_coordinates[1]
+            ].distance_to_current_node = terrain_map[node_to_expand_coordinates[0] * number_columns + node_to_expand_coordinates[1]].distance_to_current_node + 1
+            coordinates_expanded_nodes.add(((node_to_expand_coordinates[0] + 1), node_to_expand_coordinates[1]))
+
       if node_to_expand_coordinates[1] > 0:
         if (
           terrain_map[
             node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] - 1)
           ].height >= terrain_map[node_to_expand_coordinates[0] * number_columns + node_to_expand_coordinates[1]].height - 1
         ):
-          if terrain_map[node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] - 1)].explored == False:
-            terrain_map[node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] - 1)].explored = True
-            terrain_map[node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] - 1)].parent_coordinates = node_to_expand_coordinates
-            terrain_map[
-              node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] - 1)
-            ].distance_to_current_node = terrain_map[node_to_expand_coordinates[0] * number_columns + node_to_expand_coordinates[1]].distance_to_current_node + 1
-            coordinates_expanded_nodes.add((node_to_expand_coordinates[0], (node_to_expand_coordinates[1] - 1)))
-
           if (
             terrain_map[node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] - 1)].explored == True
             and terrain_map[
@@ -104,20 +110,20 @@ def bread_first_search(destination_coordinates, terrain_map, number_rows, number
             ].distance_to_current_node = terrain_map[node_to_expand_coordinates[0] * number_columns + node_to_expand_coordinates[1]].distance_to_current_node + 1
             coordinates_expanded_nodes.add((node_to_expand_coordinates[0], (node_to_expand_coordinates[1] - 1)))
 
-      if node_to_expand_coordinates[1] > number_columns - 1:
+          if terrain_map[node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] - 1)].explored == False:
+            terrain_map[node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] - 1)].explored = True
+            terrain_map[node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] - 1)].parent_coordinates = node_to_expand_coordinates
+            terrain_map[
+              node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] - 1)
+            ].distance_to_current_node = terrain_map[node_to_expand_coordinates[0] * number_columns + node_to_expand_coordinates[1]].distance_to_current_node + 1
+            coordinates_expanded_nodes.add((node_to_expand_coordinates[0], (node_to_expand_coordinates[1] - 1)))
+
+      if node_to_expand_coordinates[1] < number_columns - 1:
         if (
           terrain_map[
             node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] + 1)
           ].height >= terrain_map[node_to_expand_coordinates[0] * number_columns + node_to_expand_coordinates[1]].height - 1
         ):
-          if terrain_map[node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] + 1)].explored == False:
-            terrain_map[node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] + 1)].explored = True
-            terrain_map[node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] + 1)].parent_coordinates = node_to_expand_coordinates
-            terrain_map[
-              node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] + 1)
-            ].distance_to_current_node = terrain_map[node_to_expand_coordinates[0] * number_columns + node_to_expand_coordinates[1]].distance_to_current_node + 1
-            coordinates_expanded_nodes.add((node_to_expand_coordinates[0], (node_to_expand_coordinates[1] + 1)))
-
           if (
             terrain_map[node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] + 1)].explored == True
             and terrain_map[
@@ -129,6 +135,15 @@ def bread_first_search(destination_coordinates, terrain_map, number_rows, number
               node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] + 1)
             ].distance_to_current_node = terrain_map[node_to_expand_coordinates[0] * number_columns + node_to_expand_coordinates[1]].distance_to_current_node + 1
             coordinates_expanded_nodes.add((node_to_expand_coordinates[0], (node_to_expand_coordinates[1] + 1)))
+
+          if terrain_map[node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] + 1)].explored == False:
+            terrain_map[node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] + 1)].explored = True
+            terrain_map[node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] + 1)].parent_coordinates = node_to_expand_coordinates
+            terrain_map[
+              node_to_expand_coordinates[0] * number_columns + (node_to_expand_coordinates[1] + 1)
+            ].distance_to_current_node = terrain_map[node_to_expand_coordinates[0] * number_columns + node_to_expand_coordinates[1]].distance_to_current_node + 1
+            coordinates_expanded_nodes.add((node_to_expand_coordinates[0], (node_to_expand_coordinates[1] + 1)))
+
     coordinates_nodes_to_expand = []
 
 def exercise_1():
@@ -150,10 +165,9 @@ def exercise_1():
 
         if character == "S":
           terrain_map[current_row * number_columns + current_column] = Node(current_row, current_column, 0, start_node=True)
-          starting_coordinates = (current_row, current_column)
         
         elif character == "E":
-          terrain_map[current_row * number_columns + current_column] = Node(current_row, current_column, 26, destination_node=True)
+          terrain_map[current_row * number_columns + current_column] = Node(current_row, current_column, 25, destination_node=True)
           destination_coordinates = (current_row, current_column)
 
         else:
@@ -165,12 +179,44 @@ def exercise_1():
   
   number_rows = current_row
   
-  print(f"starting_coordinates: {starting_coordinates}")
-  print(f"destination_coordinates: {destination_coordinates}")
-  print(f"Start node: {terrain_map[starting_coordinates[0] * number_columns + starting_coordinates[1]].height}")
-  print(f"Destination node: {terrain_map[destination_coordinates[0] * number_columns + destination_coordinates[1]].height}")
+  start_node_coordinates = bread_first_search(destination_coordinates, terrain_map, number_rows, number_columns, 1)
+
+  print(f"The number of steps is = {terrain_map[start_node_coordinates[0] * number_columns + start_node_coordinates[1]].distance_to_current_node}")
+
+def exercise_2():
+
+  terrain_map = {}
   
-  start_node_coordinates = bread_first_search(destination_coordinates, terrain_map, number_rows, number_columns)
+  current_row = 0
+  current_column = 0
+  number_columns = 0
+  number_rows = 0
+
+  with open(INPUT_FILE) as input_file:
+    for line in input_file:
+      current_column = 0
+      line = line.rstrip()
+      for character in line:
+        if current_row == 0:
+          number_columns = len(line)
+
+        if character == "S":
+          terrain_map[current_row * number_columns + current_column] = Node(current_row, current_column, 0, start_node=True)
+        
+        elif character == "E":
+          terrain_map[current_row * number_columns + current_column] = Node(current_row, current_column, 25, destination_node=True)
+          destination_coordinates = (current_row, current_column)
+
+        else:
+          terrain_map[current_row * number_columns + current_column] = Node(current_row, current_column, ord(character) - ord('a'))
+
+        current_column += 1
+
+      current_row += 1
+  
+  number_rows = current_row
+  
+  start_node_coordinates = bread_first_search(destination_coordinates, terrain_map, number_rows, number_columns, 2)
 
   print(f"The number of steps is = {terrain_map[start_node_coordinates[0] * number_columns + start_node_coordinates[1]].distance_to_current_node}")
 
