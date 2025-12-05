cbb24708b8d3e3cc25e5545a7397094cf75aedc1
Afonso Luz <avsluz@gmail.com>
2025-12-05 11:21:26 -0100

    Add solution to day 3 2025

---
+++ 2025/code/day_03_lobby.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+import platform
+
+INPUT_FILE_PATH = f"{Path(__file__).parent}/../input_files/day_03_input.txt"
+
+def part_1():
+    joltage_counter = 0
+
+    with open(INPUT_FILE_PATH) as f:
+        for battery_bank in f:
+            battery_bank = battery_bank.rstrip()
+            (max, index) = find_maximum_cell(battery_bank)
+            if index != len(battery_bank) - 1:
+                (second_max, _) = find_maximum_cell(battery_bank[index + 1:])
+            else:
+                second_max = max
+                (max, _) = find_maximum_cell(battery_bank[:-1])
+            
+            joltage_counter += int(str(max) + str(second_max))
+
+    print(joltage_counter)
+
+def part_2():
+    if platform.architecture()[0] == '32bit':
+        raise OverflowError("This code can only run on 64bit systems")
+
+    BATTERY_SIZE = 12
+    joltage_counter = 0
+
+    with open(INPUT_FILE_PATH) as f:
+        for battery_bank in f:
+            battery_bank = battery_bank.rstrip()
+
+            current_battery = battery_bank[-BATTERY_SIZE:]
+            current_max = int(current_battery[0])
+            for index in range(-BATTERY_SIZE - 1, -len(battery_bank) - 1, -1):
+                if int(battery_bank[index]) >= current_max:
+                    for i in range(1,12):
+                        if int(current_battery[i]) <= current_max:
+                            temp = int(current_battery[i])
+                            current_battery = current_battery[:i] + str(current_max) + current_battery[i+1:]
+                            current_max = temp
+                        else:
+                            break
+                    
+                    current_battery =  battery_bank[index] + current_battery[1:]
+                    current_max = int(battery_bank[index])
+
+            joltage_counter += int(current_battery)
+
+    print(joltage_counter)
+
+
+def find_maximum_cell(battery_bank: str) -> tuple[int, int]:
+    max = 0
+    max_index = 0
+
+    for index, cell in enumerate(battery_bank):
+        if int(cell) > max:
+            max = int(cell)
+            max_index = index
+
+    return max, max_index
+
+if __name__ == "__main__":
+    part_2()
